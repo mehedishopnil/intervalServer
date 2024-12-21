1dba216735c45cbf0780b6d392b3e71cbe917166
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-12-21 19:52:45 +0600

    new update for the all users

--- index.js
+++ index.js
@@ -25,14 +25,17 @@ const client = new MongoClient(uri, {
 });
 
 // Database and collection variables
-let db, ResortDataCollection;
+let db, ResortDataCollection, UserDataCollection;
 
 // Lazy connection function
 async function getDatabase() {
   if (!db) {
     await client.connect();
+
     db = client.db("Interval");
     ResortDataCollection = db.collection("AllResorts");
+    UserDataCollection = db.collection("users");
+
     console.log("MongoDB connected lazily.");
   }
 }
@@ -42,6 +45,20 @@ async function getDatabase() {
 app.get('/', (req, res) => {
   res.send('Interval Server is running');
 });
+
+// Fetch all resort data
+app.get('/all-users', async (req, res) => {
+  try {
+    await getDatabase(); // Ensure the database is connected
+    const resorts = await UserDataCollection.find().toArray();
+    res.status(200).send(resorts);
+  } catch (error) {
+    console.error("Error fetching resort data:", error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+});
+
+
 
 // Fetch all resort data
 app.get('/resort-data', async (req, res) => {
